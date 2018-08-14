26f3f0c70463ba65b748dbe1962d16730e245387
LemonFridge <31214565+LemonFridge@users.noreply.github.com>
2018-08-15 01:43:09 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "Nova Networks";
+var l_serverName = "The Anarchists";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
@@ -50,7 +50,8 @@ var l_background = "backgrounds/videos/glimmering.webm";
 Place one or more images
 Multiple images will automatically shuffle between them*/
 var l_bgImages = [
-	"backgrounds/images/police.jpg"
+	"backgrounds/images/purge1.jpg"
+	"backgrounds/images/purge2.jpg"
 ];
 
 /*Random background images order?*/
